47c04f667bdad85ea9027675f429495d6462cc9c
Silas Cardoso <silasnunes105@gmail.com>
2025-09-13 13:23:04 -0300

    Ajuste webhook

--- src/main/java/com/meu_casamento/meu_casamento/controller/WebhookPagamentoController.java
+++ src/main/java/com/meu_casamento/meu_casamento/controller/WebhookPagamentoController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
-@RequestMapping("/webhooks")
+@RequestMapping("/api/webhooks")
 public class WebhookPagamentoController {
 
     private final WebhookPagamentoService service;
