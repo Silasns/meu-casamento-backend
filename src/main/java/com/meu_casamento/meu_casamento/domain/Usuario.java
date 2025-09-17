9a2720a6390b09d956f499cf338ba23988edbf04
Silas Cardoso <silasnunes105@gmail.com>
2025-09-17 17:05:13 -0300

    Ajuste enum meio pagamento

--- src/main/java/com/meu_casamento/meu_casamento/domain/Usuario.java
+++ src/main/java/com/meu_casamento/meu_casamento/domain/Usuario.java
@@ -57,6 +57,6 @@ public class Usuario {
     private List<UUID> produtosReservados = new ArrayList<>();
     
     public enum MeioReserva {
-        lojas, card
+        lojas, pagamentoDireto
     }
 }
